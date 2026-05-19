b5d9c05109b21e92e00856b8f8505c0d04395e25
AyushJoshi1403 <ayush.aj1403@gmail.com>
2026-05-19 23:56:37 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const cityInput = document.getElementById("city-input");
+  const getWeatherBtn = document.getElementById("get-weather-btn");
+  const weatherInfo = document.getElementById("weather-info");
 });
